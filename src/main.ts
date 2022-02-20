795e37538a64beca8bebc134c9a82fdc3ad5617c
Shingo Yamazaki <shingoyamazaki00@gmail.com>
2022-02-21 03:03:52 +0900

    feat: wip

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,35 @@
-function run() {}
+function run() {
+  const events = fetchEvents();
+  const durationInHoursByColor = aggregateDurationsByColor(events);
+
+  writeToSpeadSheet(durationInHoursByColor);
+}
+
+type EventColor = GoogleAppsScript.Calendar.EventColor;
+
+function fetchEvents() {
+  const today = new Date();
+  const events = CalendarApp.getEventsForDay(today);
+  return events;
+}
+
+function aggregateDurationsByColor(
+  events: GoogleAppsScript.Calendar.CalendarEvent[]
+) {
+  const durationInHoursByColor = new Map<string, number>();
+  events.forEach((event) => {
+    const color = event.getColor();
+    const durationInHours =
+      ((event.getEndTime().getTime() - event.getStartTime().getTime()) / 60) *
+      60 *
+      1000;
+    const totalDurationInHours =
+      durationInHours + (durationInHoursByColor.get(color) || 0);
+    durationInHoursByColor.set(color, totalDurationInHours);
+  });
+  console.log(durationInHoursByColor);
+
+  return durationInHoursByColor;
+}
+
+function writeToSpeadSheet(durationInHoursByColor: Map<string, number>) {}
