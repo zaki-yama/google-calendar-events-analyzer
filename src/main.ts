cf6035f4c766bd7f8b90ccfb1fcc17bf1d889551
Shingo Yamazaki <shingoyamazaki00@gmail.com>
2022-08-31 00:09:37 +0900

    feat: calculate duration

--- src/main.ts
+++ src/main.ts
@@ -10,6 +10,9 @@ type EventColor = GoogleAppsScript.Calendar.EventColor;
 function fetchEvents() {
   const today = new Date();
   const events = CalendarApp.getEventsForDay(today);
+  // TODO: filter events so that:
+  // - include only accepted events
+  // - ignore allday events
   return events;
 }
 
@@ -18,18 +21,56 @@ function aggregateDurationsByColor(
 ) {
   const durationInHoursByColor = new Map<string, number>();
   events.forEach((event) => {
-    const color = event.getColor();
+    const color = event.getColor() || "default";
+    console.log(`----${event.getTitle()} (${event.getColor()}`);
     const durationInHours =
-      ((event.getEndTime().getTime() - event.getStartTime().getTime()) / 60) *
-      60 *
-      1000;
+      (event.getEndTime().getTime() - event.getStartTime().getTime()) /
+      (60 * 60 * 1000);
+    console.log("duration", durationInHours);
     const totalDurationInHours =
       durationInHours + (durationInHoursByColor.get(color) || 0);
+    console.log("totalDuration", totalDurationInHours);
     durationInHoursByColor.set(color, totalDurationInHours);
   });
-  console.log(durationInHoursByColor);
+  console.log(durationInHoursByColor.values);
 
   return durationInHoursByColor;
 }
 
-function writeToSpeadSheet(durationInHoursByColor: Map<string, number>) {}
+function writeToSpeadSheet(durationInHoursByColor: Map<string, number>) {
+  const ss = SpreadsheetApp.getActiveSpreadsheet();
+  const sheet = ss.getSheetByName("data");
+  if (!sheet) {
+    throw new Error("data sheet not found");
+  }
+  const range = sheet.getDataRange();
+  console.log(range.getNumRows(), range.getNumColumns());
+  const row = sheet.getRange(
+    range.getNumRows() + 1,
+    1,
+    1,
+    range.getNumColumns()
+  );
+  const headers = sheet.getRange(1, 1, 1, range.getNumColumns()).getValues()[0];
+  const values = headers.map((header) => {
+    return durationInHoursByColor.get(eventTypes[header]);
+  });
+  // @ts-ignore
+  values[0] = new Date();
+  console.log("values", values);
+  row.setValues([values]);
+}
+
+const eventTypes = {
+  その他: "default",
+  CRE: "7",
+  定例: "8",
+};
+
+type CalendarEvent = {
+  title: string;
+  startTime: number;
+  endTime: number;
+  duration: number;
+  color: string;
+};
